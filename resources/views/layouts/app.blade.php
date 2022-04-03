83b20f937c8e5cdf628f655f4cf0add8d51036de
tadokoro <tadokoro0216@gmail.com>
2022-04-03 18:43:10 +0000

    validation

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,6 +25,8 @@
         </header>
         
         <div class="container">
+            @include('commons.error_messages')
+            
             @yield('content')
         </div>
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
